780f2ebb60728a632f56d481b7c891585f1f3677
Adam Reidelbach <adamreidelbach@gmail.com>
2017-08-29 11:34:36 -0500

    first commit

---
+++ BangazonSite/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using Bangazon.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonSite.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set;
+
+        [Required]
+        public virtual ApplicationUser User { get; set; }
+
+        [Required]
+        public int PaymentTypeId { get; set; }
+
+        [Display(Name = "PaymentType")]
+        public PaymentType PaymentTypeId { get; set; }
+    }
+}
