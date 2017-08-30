cc6baaa3563b171417edb511b3a820cb2a5fd3c8
joeybiotti <joeybiotti4@gmail.com>
2017-08-30 13:19:03 -0500

    user email displays in navbar

--- BangazonSite/Models/AccountViewModels/RegisterViewModel.cs
+++ BangazonSite/Models/AccountViewModels/RegisterViewModel.cs
@@ -19,6 +19,18 @@ namespace BangazonSite.Models.AccountViewModels
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
