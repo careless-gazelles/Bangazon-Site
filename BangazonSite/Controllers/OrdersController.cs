66a7a0deddaad314d0d638ae4cfc789938af7604
ollieOsh <sosinusi@gmail.com>
2017-09-01 09:37:29 -0500

    hopefully won't have to burn this one :)

--- BangazonSite/Controllers/OrdersController.cs
+++ BangazonSite/Controllers/OrdersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BangazonSite.Data;
 using BangazonSite.Models;
+using BangazonSite.Models.OrderViewModels;
 
 namespace BangazonSite.Controllers
 {
---
+++ BangazonSite/Models/OrderViewModels/OrderDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonSite.Models.OrderViewModels
+{
+    public class OrderDetailViewModel
+    {
+        public List<Product> Products { get; set; }
+        public Order Order { get; set; }
+    }
+}
