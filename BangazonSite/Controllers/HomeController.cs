73ca86353ebd1576f60b418468e9083b9ff3b376
Adam Reidelbach <adamreidelbach@gmail.com>
2017-09-01 11:59:30 -0500

    searching logic in place, but nothing to see

--- BangazonSite/Controllers/HomeController.cs
+++ BangazonSite/Controllers/HomeController.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BangazonSite.Data;
 
 namespace BangazonSite.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HomeController (ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        //public IActionResult Index()
+        public ViewResult Index(string searchString)
+        {
+            var products = from p in _context.Product
+                           select p;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                products = products.Where(p => p.Title.Contains(searchString)
+                                       || p.Description.Contains(searchString));
+            }
+
+            return View(products.ToList());
         }
 
         public IActionResult About()
